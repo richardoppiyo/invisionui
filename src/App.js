9122b525ba7a77b1eca71fe6ae670aef2d86b7f4
Richard Opiyo <romenyo@dewcis.com>
2022-04-24 19:47:42 +0300

    finishing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
-// import NameForm from './form';
+import NameForm from './form';
+import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineWindows } from "react-icons/ai";
+import { FiMessageSquare } from "react-icons/fi";
+import { VscCode } from "react-icons/vsc";
 
 function App() {
   return (
@@ -13,15 +17,22 @@ function App() {
             </p>
           </div>
           <div className="lower">
-            <p>inner content</p>
-            {/* <NameForm /> */}
+            <NameForm />
           </div>
         </div>
         <div className="right-div">
-          <h1>Dummy heading</h1>
-          <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but
-          </p>
+          <div className="right-upper-div">
+            <h1>Dummy heading</h1>
+            <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but
+            </p>
+          </div>
+          <div className="right-lower-div">
+            <AiOutlineSearch className="icon1" />
+            <FiMessageSquare className="icon1"/>
+            <VscCode className="icon1"/>
+            <AiOutlineWindows className="icon1"/>
+         </div>
         </div>
         </div>
       </header>
--- src/form.js
+++ src/form.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { ReactDOM } from "react";
-
+import './form.css';
 
 class NameForm extends React.Component {
     constructor(props) {
       super(props);
       this.state = {value: ''};
-  
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -22,33 +20,42 @@ class NameForm extends React.Component {
   
     render() {
       return (
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit} className="form-div">
+            <div>
+                <h1>Let's set up your account</h1>
+            </div>
+            <div>
+                <p>Already have an acoount? 
+                <span><a href="/">Sign In</a></span>
+                </p>
+            </div>
           <div>
-            <input type="text" value={this.state.value} onChange={this.handleChange} placeholder="Name"/>
+            <input className="form-element" type="text" onChange={this.handleChange} placeholder="Your name"/>
           </div>
           <div>
-            <input type="text" value={this.state.value} onChange={this.handleChange} placeholder="Email"/>
+            <input className="form-element" type="text" onChange={this.handleChange} placeholder="Email address"/>
           </div>
-            <select value={this.state.value} onChange={this.handleChange}>
+            <select className="form-element2" onChange={this.handleChange}>
                <option value="none">I would describe my user type as</option>
                 <option value="Superuser">Superuser</option>
-                <option value="admin">admin</option>
+                <option value="admin">Admin</option>
                 <option value="finance">finance</option>
-                <option value="member">member</option>
+                <option value="member">Member</option>
              </select>
           <div>
-            <input type="password" value={this.state.value} onChange={this.handleChange} placeholder="password"/>
+            <input className="form-element" type="password" onChange={this.handleChange} placeholder="password"/>
           </div>
      
-          <input type="submit" value="Submit" />
+          <input className="form-element3" type="submit" value="Next" />
+          <div className="form-element3" >
+                <p className="form-text">By clicking the "Next" button, you agree to creating a free acoount, and To <a href="/</p>">Terms of service</a> and  
+             <a href="/"> Privacy Policy</a></p>
+        </div>
         </form>
       );
     }
   }
   
-  ReactDOM.render(
-    <NameForm />,
-    document.getElementById('root')
-  );
+ 
 
   export default NameForm;
